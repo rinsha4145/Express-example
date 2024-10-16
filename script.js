98bdd5818ee632c95b501b21dda1fd5b80b88dbb
rinsha4145 <rinshajouhar5181@gmail.com>
2024-10-16 10:30:49 +0530

    first

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const path =require('path')
 
 app.get('/', (req, res) => {
-    res.send('<h1>Home</h1>');
+    res.sendFile(path.join(__dirname,'view',"index.html"));
 });
 
 app.get('/contact', (req, res) => {
