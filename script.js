bafbeb65728fd540c23473f8661ebd8d34015398
rinsha4145 <rinshajouhar5181@gmail.com>
2024-10-16 10:35:15 +0530

    first

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const app = express();
-const path =require('path')
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'view',"index.html"));
-});
+app.use(express.static('view'));
 
-app.get('/contact', (req, res) => {
-    res.send('<h1>Contact</h1>');
-});
+// const path =require('path')
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname,'view',"index.html"));
+// });
+
+// app.get('/contact', (req, res) => {
+//     res.send('<h1>Contact</h1>');
+// });
 
 app.get('*', (req, res) => {
-    res.status(404)
-    res.send('<h1>404</h1>');
+    res.status(404).send('<h1>404</h1>');
 });
 
 const PORT = process.env.PORT || 3001;
