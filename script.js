9c23c867ff06cf3130b3a4ef443ba23fb16bc93a
rinsha4145 <rinshajouhar5181@gmail.com>
2024-10-16 11:08:26 +0530

    first

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.use(express.static('view'));
+// app.use(express.static('view'));
 
 // const path =require('path')
 
@@ -13,9 +13,32 @@ app.use(express.static('view'));
 //     res.send('<h1>Contact</h1>');
 // });
 
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404</h1>');
-});
+// app.get('*', (req, res) => {
+//     res.status(404).send('<h1>404</h1>');
+// });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+var express = require('express');  
+var app = express();  
+app.use(express.static('public'));  
+  
+app.get('/index.html', function (req, res) {  
+   res.sendFile( __dirname + "/" + "index.html" );  
+})  
+app.get('/process_get', function (req, res) {  
+response = {  
+       first_name:req.query.first_name,  
+       last_name:req.query.last_name  
+   };  
+   console.log(response);  
+   res.end(JSON.stringify(response));  
+})  
+var server = app.listen(8000, function () {  
+  
+  var host = server.address().address  
+  var port = server.address().port  
+  console.log("Example app listening at http://%s:%s", host, port)  
+  
+})  
